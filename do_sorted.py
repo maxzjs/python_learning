686d2cc32687ccde37c292dde25c93d3a3e6ebcc
maxzjs <626703431@qq.com>
2018-09-30 23:07:01 +0800

    sorted练习解答、添加函数返回值
    sorted练习解答ok

--- do_sorted.py
+++ do_sorted.py
@@ -1,4 +1,5 @@
 #2018-9-28
+import os
 # 高级函数之 sorted 排序函数
 # sorted 可以对list进行排序
 # 可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序：
@@ -6,14 +7,29 @@ sorted([36,5,-12,9,-21])
 sorted([36,5,-12,9,-21],key=abs)
 
 #字符串排序，是按照ASCII的大小比较的，通过key函数把字符串映射为忽略大小写排序即可
-sorted([36,5,-12,9,-21],key=str.lower)
+'''
+sorted([36,5,-12,9,-21],key=str.lower) 有问题，会报错
+'''
 #反向排序，传入第三个参数
-sorted([36,5,-12,9,-21],key=str.lower,reverse=True)
+'''
+sorted([36,5,-12,9,-21],key=str.lower,reverse=True) 会报错
+'''
 #高阶函数的抽象能力非常强大。
-
+# -*- coding:utf-8 -*- 
 #练习 
 # 用一组tuple表示学生名字和成绩：
+
 L = [('Bob',75),('Adam',92),('Bart',66),('Lisa',88)]
+
 # 请用sorted对上述列表分别按名字排序
+def by_name(t):
+	return t[0]
+L2 = sorted(L, key=by_name)
+print(L2)
+
 # 在按成绩从高到底排序
-# -*- coding:utf-8 -*- 
+def by_score(q):
+	return q[1]
+L2 = sorted(L, key=by_score,reverse=True)   #reverse为真时，反向，默认是从小到大排
+print(L2)
+os.system('pause')
---
+++ 函数返回值.py
@@ -0,0 +1,86 @@
+# 2018-9-30 明天国庆了
+# -*- coding:utf-8 -*- 
+import os
+# 函数返回值 除了可以接收函数作为参数外，还可以把函数作为结果值返回
+
+# 实现一个可变参数的求和
+
+def calc_sum(*args):
+	ax = 0
+	for n in args:
+		ax = ax + n 
+	return ax
+
+# 如果不需要立刻求和 ，而是在后面的代码中，根据需要再计算，返回求和的函数
+
+def lazy_sum(*args):
+		def sum():
+			axa = 0
+			for na in args:
+				axa = axa + na
+			return axa
+		return sum
+
+f = lazy_sum(1,3,5,7,9)
+print(f())					
+'''
+在函数lazy_sum中又定义函数sum，内部函数sum可以引用外部函数的参数和局部变量，外函数返回函数sum，相关参数和变量都保存在返回的函数中，这就是闭包
+
+！！ 调用闭包函数时，每次调用都会返回一个新的函数
+
+返回函数不要引用任何循坏变量，
+'''
+f1 = lazy_sum(1,3,5,7,9)
+f2 = lazy_sum(1,3,5,7,9)
+print(f1==f2)
+'''
+'''
+def count():
+	fs = []
+	for i in range(1,4):
+		def ff():
+			return i * i
+		fs.append(ff)
+	return fs
+f11,f22,f33 = count()
+print(f11(),f22(),f33())
+'''
+返回的函数引用了变量i，等到3个函数都返回时，他们所引用的变量i已经变成了3
+
+
+一定要引用循环变量咋整？ 再创建一个函数，用该函数的参数绑定循环变量当前的值，无论该循环变量后续如何更改，已绑定掉函数参数的值不变
+'''
+def count1():
+	def fff(j):
+		def g():
+			return j * j
+		return g
+	fss = []
+	for x in range(1,4):
+		fss.append(fff(x))
+	return fss
+g1,g2,g3 = count1()
+print(g1(),g2(),g3())
+'''
+练习
+利用闭包返回一个计数器函数，每次调用它返回递增整数
+'''
+
+def createCounter():
+	def counter(q):
+		def cc():
+			return  q + 1
+		return cc
+	ccf = []
+	for nn in range(1,100):
+		ccf.append(counter(nn))
+	return ccf
+
+#counterA = createCounter()
+
+# 有问题，返回值不对
+
+
+
+
+os.system('pause')
