71c2491ae6fa26f72948afe30e4e6feaf7eded5d
Virtualzjs <13843321+maxzjs@users.noreply.github.com>
2018-10-16 23:45:23 +0800

    完成ok

--- 访问限制.py
+++ 访问限制.py
@@ -18,9 +18,14 @@ class students(object):
 		return self.__name
 	def get_score(self):
 		return self.__score
+
+	def set_name(self,name):
+		self.__name = name
+	def set_score(self,score):
+		self.__score = score
 # 又要允许代码修改score，增加方法：
 # 在方法中对参数做检查，避免传入无效或者错误的参数
-	def get_score1(self,score):
+	def set_score1(self,score):
 		if 0 <= score <= 100:
 			self.__score = score
 		else:
@@ -37,12 +42,20 @@ class Stu(object):
 	def __init__(self, name,gender):
 		self.__name = name
 		self.__gender = gender
-	def get_name1(self):
-		return self.__name
 	def get_gender(self):
 		return self.__gender
-		
-# 练习不对
+	def set_gender(self,gender):
+		self.__gender = gender	
 
-
+bart = Stu('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
+# 解决ok
+# 和C#的构建私有变量方法道理一样。。只是把 get和set分开写到两个def中
 os.system('pause')
