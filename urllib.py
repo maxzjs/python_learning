069c8cfdfcdb637647473bef4dde667ee8ba4808
Virtualzjs <13843321+maxzjs@users.noreply.github.com>
2018-12-12 22:54:29 +0800

    urllib模块

---
+++ urllib.py
@@ -0,0 +1,14 @@
+# 2018.12.12
+# maxzjs
+# -*- coding:utf-8 -*-
+# urllib 操作URL功能
+# Get
+
+from urllib import request
+
+with request.urlopen('https://api.douban.com/v2/book/2129650') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
