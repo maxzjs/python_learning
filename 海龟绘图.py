d97f618126d09011860a625e3004bdd4a2c8beda
Virtualzjs <13843321+maxzjs@users.noreply.github.com>
2018-12-16 18:05:14 +0800

    turtle 模块，图形界面

---
+++ 海龟绘图.py
@@ -0,0 +1,34 @@
+# 2018.12.16
+# maxzjs
+# -*- coding:utf-8 -*-
+# 海龟绘图
+
+from turtle import *
+
+# 设置宽度
+width(4)
+
+
+# 前进
+forward(200)
+# 右转90度
+right(90)
+
+
+# 笔刷颜色
+pencolor('red')
+forward(100)
+right(90)
+
+pencolor('green')
+forward(200)
+right(90)
+
+pencolor('blue')
+forward(100)
+right(90)
+
+# 调用done使得窗口等待被关闭，否则将立刻关闭窗口
+done()
+
+
