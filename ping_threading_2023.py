a7504ec666210c2c5acef288b6e6e0a2b31b4955
Virtualzjs <626703431@qq.com>
2023-04-10 18:03:12 +0800

    更新
    多线程ping多个ip地址改为输入ip和掩码自动计算本网段数并批量ping

--- ping_threading_2023.py
+++ ping_threading_2023.py
@@ -1,40 +1,63 @@
 # 2018.12.11
-# maxzjs
+# 更新时间：2023.4.10
+# 作者：maxzjs
 # 批量ping 电脑主机ip
 # -*- coding:utf-8 -*-
 
 import threading
 import subprocess
 import time
-import os,sys
+import ipaddress
+import os, sys
 from queue import Queue
 
+
+
 # 定义工作线程
-WORD_THREAD = 50
+WORD_THREAD = 10
 
 # 将需要 ping 的 ip 加入队列
 IP_QUEUE = Queue()
 
 
 
+# 判断ip地址是否正确
+def judge_ip(ip):
+    ip_str = ip
+    ip_list = ip_str.split(".")  # 将字符串按点分割成列表
+    for num in ip_list:
+        if len(ip_list) == 4 and num.isdigit() and 0 <= int(num) <= 255:
+            continue
+        else:
+            ip = input("请输入ip地址：")
+            return judge_ip(ip)
 
 
-for i in range(1,255):
-    IP_QUEUE.put('10.3.33.'+str(i))
+
+i_ip = input("请输入ip地址：")
+i_mask = input("请输入子网掩码：")
+jud = judge_ip(i_ip)
+i_ip_add = i_ip + '/' + i_mask
+ip_net_add = ipaddress.ip_interface(i_ip_add).network
+ipadd_net = ipaddress.ip_network(str(ip_net_add))
+for n in ipadd_net.hosts():
+    IP_QUEUE.put(str(n))
 
 # 定义一个执行 ping 的函数
 def ping_ip():
     while not IP_QUEUE.empty():
         ip = IP_QUEUE.get()
-        res = subprocess.call('ping -n 2 -w 5 %s' % ip,stdout=subprocess.PIPE)  # linux 系统将 '-n' 替换成 '-c'
+        res = subprocess.call('ping -n 2 -w 5 %s' % ip, stdout=subprocess.PIPE)  # linux 系统将 '-n' 替换成 '-c'
         # 打印运行结果
         # 判断ip通不通，通着打印，不通丢弃
         if res != 0:
             pass
         else:
-            print(ip,True if res == 0 else False)
+            print(ip, True if res == 0 else False)
+
 
 if __name__ == '__main__':
+    print("批量ping多个IP地址")
     threads = []
     start_time = time.time()
     for i in range(WORD_THREAD):
@@ -46,3 +69,5 @@ if __name__ == '__main__':
         thread.join()
 
     print('程序运行耗时：%s' % (time.time() - start_time))
+
+
